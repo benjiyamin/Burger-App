e9b43c4901d46622832694dbb196305756b52541
Ben Miller <mail@benkmiller.com>
2019-06-18 17:41:18 -0400

    JAWSDB_URL in production

--- app/config/connection.js
+++ app/config/connection.js
@@ -3,13 +3,20 @@ const mysql = require('mysql')
 const keys = require('../../keys')
 
 
-let connection = mysql.createConnection({
-  host: keys.mysql.host,
-  port: keys.mysql.port,
-  user: keys.mysql.user,
-  password: keys.mysql.password,
-  database: keys.mysql.database
-})
+let opts
+if (keys.mysql.jawsdb_url) {
+  opts = keys.mysql.jawsdb_url
+} else {
+  opts = {
+    host: keys.mysql.host,
+    port: keys.mysql.port,
+    user: keys.mysql.user,
+    password: keys.mysql.password,
+    database: keys.mysql.database
+  }
+}
+
+let connection = mysql.createConnection(opts)
 
 connection.connect(function (error) {
   if (error) throw error
--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 exports.mysql = {
+  jawsdb_url: process.env.JAWSDB_URL,
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
